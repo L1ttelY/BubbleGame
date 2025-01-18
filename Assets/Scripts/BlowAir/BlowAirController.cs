7e6959097d22eba65bf5db75c45687a18f4d4ab7
L1ttelY <42773826+L1ttelY@users.noreply.github.com>
2025-01-18 23:51:05 +0800

    吹风

--- Assets/Scripts/BlowAir/BlowAirController.cs
+++ Assets/Scripts/BlowAir/BlowAirController.cs
@@ -6,21 +6,65 @@ public class BlowAirController:MonoBehaviour {
 
 	public static BlowAirController instance;
 	public float blowTime = 1f;
-	float timeAfterBlow;
+	float timeAfterBlow = float.MaxValue;
+	FlowingArea flowArea;
 
 	[SerializeField] GameObject arrowObject;
+	[SerializeField] float blowSpeed;
 
+	enum State {
+		Choosing,
+		Working,
+		Idle
+	}
+	State currentState = State.Idle;
 
 	private void Start() {
 		instance=this;
+		flowArea=GetComponentInChildren<FlowingArea>();
 	}
 
 	private void Update() {
 		timeAfterBlow+=Time.deltaTime;
-		if(timeAfterBlow>blowTime) {
-			transform.position=Vector3.left*100000000f;
-		}
 
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		switch(currentState) {
+		case State.Choosing:
+			Angle angle = new Angle(mousePosition-(Vector2)arrowObject.transform.position);
+			arrowObject.transform.rotation=angle.quaternion;
+			if(!Input.GetMouseButton(0)) {
+				transform.position=arrowObject.transform.position;
+				transform.rotation=arrowObject.transform.rotation;
+
+				arrowObject.transform.position=Vector3.left*100000f;
+				timeAfterBlow=0;
+				flowArea.velocity=angle.vector*blowSpeed;
+
+				currentState=State.Working;
+			}
+			break;
+		case State.Working:
+			timeAfterBlow+=Time.deltaTime;
+			if(timeAfterBlow>blowTime) {
+				transform.position=Vector3.left*100000f;
+				arrowObject.transform.position=Vector3.left*100000f;
+				currentState=State.Idle;
+			}
+			break;
+		case State.Idle:
+
+			transform.position=Vector3.left*100000f;
+			arrowObject.transform.position=Vector3.left*100000f;
+
+			if(Input.GetMouseButtonDown(0)) {
+				if(!Physics2D.OverlapPoint(mousePosition,LayerMask.GetMask("Collider","ColliderSticky","Spike","Bubble","BubbleElastic","Liquid"))) {
+					arrowObject.transform.position=mousePosition;
+					currentState=State.Choosing;
+				}
+			}
+			break;
+		}
 
 	}
 
--- Assets/Scripts/BubbleNodeController.cs
+++ Assets/Scripts/BubbleNodeController.cs
@@ -7,11 +7,16 @@ public class BubbleNodeController:MonoBehaviour {
 	List<Transform> points;
 	int thisIndex;
 	[SerializeField] float pressureFactor;
+	[SerializeField] float clampForce = 1;
+	[SerializeField] float relativeConstraintsThreshold = 0.5f;
+
+	RelativeJoint2D relativeConstraints;
 
 	Rigidbody2D rigidBody;
-
+	Vector2 force;
 	void Start() {
 		rigidBody=GetComponent<Rigidbody2D>();
+		relativeConstraints=GetComponent<RelativeJoint2D>();
 		points=new List<Transform>(GetComponentInParent<SoftBody>().points);
 		points.Remove(points[points.Count-1]);
 		foreach(var i in points) Debug.Log(i);
@@ -19,6 +24,7 @@ public class BubbleNodeController:MonoBehaviour {
 	}
 
 	void FixedUpdate() {
+		force=Vector2.zero;
 		Vector2 forceTotal = Vector2.zero;
 		foreach(var testFlowingArea in FlowingArea.instances) {
 			if(!testFlowingArea.collider.OverlapPoint(transform.position)) continue;
@@ -37,7 +43,16 @@ public class BubbleNodeController:MonoBehaviour {
 		}
 		if(forceTotal==Vector2.zero) forceTotal+=GetForce(Vector2.zero);
 		rigidBody.AddForce(forceTotal);
+
+		if(relativeConstraints) {
+			float currentDifferenceToTarget = (relativeConstraints.target-(Vector2)transform.position).magnitude;
+			float acceptedDifferenceToTarget = relativeConstraints.linearOffset.magnitude*relativeConstraintsThreshold;
+			if(currentDifferenceToTarget>acceptedDifferenceToTarget) relativeConstraints.enabled=true;
+			else relativeConstraints.enabled=false;
+		}
+
 	}
+
 
 	Vector2 GetForce(Vector2 localAirVelocity) {
 		Vector2 relativeVelocity = localAirVelocity-rigidBody.velocity;
@@ -51,13 +66,14 @@ public class BubbleNodeController:MonoBehaviour {
 		previousPosition=(previousPosition+thisPosition)/2f;
 		nextPosition=(nextPosition+thisPosition)/2f;
 
-		float factor1 = Utils.CrossAbs((previousPosition-thisPosition).normalized,relativeVelocity.normalized);
-		float factor2 = Utils.CrossAbs((nextPosition-thisPosition).normalized,relativeVelocity.normalized);
+		float factor1 = Utils.CrossAbs(previousPosition-thisPosition,relativeVelocity.normalized);
+		float factor2 = Utils.CrossAbs(nextPosition-thisPosition,relativeVelocity.normalized);
 		float factor = factor1+factor2;
 		if(Vector2.Dot(normal,relativeVelocity)>0) factor=0;
 
 		GetComponent<SpriteRenderer>().color=new Color(factor,factor,factor);
-		return factor*relativeVelocity*pressureFactor;
+		this.force+=factor*Vector2.ClampMagnitude(relativeVelocity,clampForce)*pressureFactor;
+		return factor*Vector2.ClampMagnitude(relativeVelocity,clampForce)*pressureFactor;
 	}
 
 	static ContactFilter2D airBlowFilter = new ContactFilter2D();
@@ -70,7 +86,7 @@ public class BubbleNodeController:MonoBehaviour {
 		Vector2 previousPosition = points[(thisIndex+points.Count-1)%points.Count].position;
 		Vector2 nextPosition = points[(thisIndex+points.Count+1)%points.Count].position;
 		Vector2 normal = Utils.Prudoct((nextPosition-previousPosition).normalized,Vector2.up);
-		Gizmos.DrawLine(transform.position,transform.position+(Vector3)normal);
+		Gizmos.DrawLine(transform.position,transform.position+(Vector3)force);
 	}
 
 }
