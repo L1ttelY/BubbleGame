122b9ff1f7a7e71363918d629783cf7ca5c71b36
L1ttelY <42773826+L1ttelY@users.noreply.github.com>
2025-01-18 21:22:51 +0800

    气流遮挡

--- Assets/Scripts/BubbleNodeController.cs
+++ Assets/Scripts/BubbleNodeController.cs
@@ -17,21 +17,29 @@ public class BubbleNodeController:MonoBehaviour {
 		foreach(var i in points) Debug.Log(i);
 		for(int i = 0;i<points.Count;i++) if(points[i]==transform) thisIndex=i;
 	}
-	void FixedUpdate() {
 
-		Vector2 localAirVelocity = Vector2.zero;
+	void FixedUpdate() {
 		Vector2 forceTotal = Vector2.zero;
 		foreach(var testFlowingArea in FlowingArea.instances) {
-			if(testFlowingArea.collider.OverlapPoint(transform.position)) {
-				forceTotal+=GetForce(testFlowingArea.velocity);
+			if(!testFlowingArea.collider.OverlapPoint(transform.position)) continue;
+			RaycastHit2D hit = Physics2D.Raycast(
+				testFlowingArea.transform.position,
+				transform.position-testFlowingArea.transform.position,
+				(transform.position-testFlowingArea.transform.position).magnitude,
+				LayerMask.GetMask("Collider","ColliderSticky","Spike","Liquid"),
+				int.MinValue
+			);
+			if(hit.collider) {
+				continue;
 			}
+			forceTotal+=GetForce(testFlowingArea.velocity);
+
 		}
 		if(forceTotal==Vector2.zero) forceTotal+=GetForce(Vector2.zero);
 		rigidBody.AddForce(forceTotal);
-
 	}
 
-	Vector2 GetForce(Vector2 localAirVelocity){
+	Vector2 GetForce(Vector2 localAirVelocity) {
 		Vector2 relativeVelocity = localAirVelocity-rigidBody.velocity;
 
 		Vector2 previousPosition = points[(thisIndex+points.Count-1)%points.Count].position;
@@ -43,12 +51,26 @@ public class BubbleNodeController:MonoBehaviour {
 		previousPosition=(previousPosition+thisPosition)/2f;
 		nextPosition=(nextPosition+thisPosition)/2f;
 
-		float factor1 = Utils.CrossAbs(previousPosition-thisPosition,relativeVelocity.normalized);
-		float factor2 = Utils.CrossAbs(nextPosition-thisPosition,relativeVelocity.normalized);
+		float factor1 = Utils.CrossAbs((previousPosition-thisPosition).normalized,relativeVelocity.normalized);
+		float factor2 = Utils.CrossAbs((nextPosition-thisPosition).normalized,relativeVelocity.normalized);
 		float factor = factor1+factor2;
 		if(Vector2.Dot(normal,relativeVelocity)>0) factor=0;
 
+		GetComponent<SpriteRenderer>().color=new Color(factor,factor,factor);
 		return factor*relativeVelocity*pressureFactor;
+	}
+
+	static ContactFilter2D airBlowFilter = new ContactFilter2D();
+	static BubbleNodeController() {
+		airBlowFilter.useLayerMask=true;
+		airBlowFilter.SetLayerMask(LayerMask.GetMask());
+	}
+
+	private void OnDrawGizmos() {
+		Vector2 previousPosition = points[(thisIndex+points.Count-1)%points.Count].position;
+		Vector2 nextPosition = points[(thisIndex+points.Count+1)%points.Count].position;
+		Vector2 normal = Utils.Prudoct((nextPosition-previousPosition).normalized,Vector2.up);
+		Gizmos.DrawLine(transform.position,transform.position+(Vector3)normal);
 	}
 
 }
